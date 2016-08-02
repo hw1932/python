ebbfcad806db599ac62cd79aa79138a5e27e1fb8
hw <947412461@qq.com>
2016-08-02 16:16:20 +0800

    2016 8 2

---
+++ Selenium/007day/JS/turn_red.py
@@ -0,0 +1,20 @@
+﻿#coding=utf-8
+from selenium import webdriver
+import time
+driver = webdriver.Firefox()
+driver.get("https://www.zhihu.com/#signin")
+
+#给用户名的输入框标红
+js='var q=document.querySelector(".email input");q.style.border=\"1px solid red\";'
+
+#调用 js
+driver.execute_script(js)
+time.sleep(3)
+
+#登录
+driver.find_element_by_name("account").send_keys("3000huangwei")
+driver.find_element_by_name("password").send_keys("a123123")
+driver.find_element_by_link_text("登录").click()
+time.sleep(3)
+
+driver.quit()
---
+++ Selenium/007day/alert/alert.py
@@ -0,0 +1,29 @@
+﻿#coding=utf-8
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+#初始化并访问百度首页
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get('https://www.baidu.com')
+
+#鼠标悬停相“设置”链接
+link = driver.find_element_by_link_text(u'设置')
+ActionChains(driver).move_to_element(link).perform()
+time.sleep(3)
+
+#打开搜索设置
+driver.find_element_by_class_name('setpref').click()
+time.sleep(3)
+
+#保存设置
+driver.find_element_by_css_selector('#gxszButton > a.prefpanelgo').click()
+time.sleep(3)
+
+#接收弹窗
+driver.switch_to_alert().accept()
+time.sleep(3)
+
+#退出
+driver.quit()
