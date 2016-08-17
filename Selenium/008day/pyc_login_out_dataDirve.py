c1c16f82c7a45a8e300fc1a953e4753b9c68f9e7
hw <947412461@qq.com>
2016-08-17 18:09:52 +0800

    8.17更新

---
+++ Selenium/008day/pyc_login_out_dataDirve.py
@@ -0,0 +1,29 @@
+﻿#coding=utf-8
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get("http://www.pyc.com.cn/login.aspx")
+
+class Account(object):
+"""docstring for Account"""
+	def __init__(self,username ='', password = ''):
+		self.username = username
+		self.password = password
+		
+def do_login_as(user_info):
+	driver.find_element_by_id("idInput").clear()
+	driver.find_element_by_id("idInput").send_keys(user_info.username)
+	driver.find_element_by_id("pwdInput").clear()
+	driver.find_element_by_id("pwdInput").send_keys(user_info.password)
+	driver.find_element_by_id("loginBtn").click()
+
+file_info = open('pyc_login_out_dataDrive_info.txt','r')
+values = file_info.readlines()
+file_info.close()
+	 
+#实例化登陆信息
+admin = Account(username='admin',password='123')
+guset = Account(username='guset',password='321')
+#调用登陆函数
+do_login_as(admin)
+do_login_as(guset)
---
+++ Selenium/008day/pyc_login_out_modules.py
@@ -0,0 +1,26 @@
+﻿#coding=utf-8
+from selenium import webdriver
+
+user = '947412461@qq.com'
+pwd = 'a123456'
+
+#登陆
+def login(driver):
+	#driver.find_element_by_id("u").clear()
+	driver.find_element_by_id("txtLogname").send_keys(user)
+	#driver.find_element_by_id("p").clear()
+	driver.find_element_by_id("txtPass").send_keys(pwd)
+	driver.find_element_by_id("btnLogin").click()
+	
+#退出
+def logout(driver):
+	driver.find_element_by_link_text("退出").click()
+	driver.quit()
+	
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+
+driver.get("http://www.pyc.com.cn/login.aspx")
+login(driver) #调用登陆模块
+#收信、写信、删除信件等操作
+logout(driver) #调用退出模块
