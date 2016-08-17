be6bfeaae355924f154bd393d75d669de210f292
hw <947412461@qq.com>
2016-08-17 18:04:31 +0800

    1

---
+++ Selenium/007day/browser_basic_event/screenshot.py
@@ -0,0 +1,13 @@
+﻿#coding=utf-8
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get('https://www.baidu.com')
+
+try:
+	driver.find_element_by_id('kw_wrong').send_key('selenium')
+	driver.find_element_by_id('su').click()
+
+except :
+	driver.get_screenshot_as_file("D:\\baidu_error.jpg")
+	driver.quit()
