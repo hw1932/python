1c69b3f8c0bd938cfbb7fc402619b947671a97d5
hw <947412461@qq.com>
2016-08-18 17:18:08 +0800

    0

--- Selenium/007day/level_locate/locate.py
+++ Selenium/007day/level_locate/locate.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+﻿#coding:utf-8
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
@@ -9,9 +9,9 @@ dr.get(file_path)
 #点击 Link1链接（弹出下拉列表）
 dr.find_element_by_link_text('Link1').click()
 #找到 id 为 dropdown1的父元素
-WebDriverWait(dr, 10).until(lambda the_driver:the_driver.find_element_by_id('dropdown1').is_displayed())
+WebDriverWait(dr, 10).until(lambda the_driver:the_driver.find_element_by_id('drop4').is_displayed())
 #在父亲元件下找到 link 为 Action 的子元素
-menu = dr.find_element_by_id('dropdown1').find_element_by_link_text('Action')
+menu = dr.find_element_by_id('drop4').find_element_by_link_text('Action')
 #鼠标定位到子元素上
 webdriver.ActionChains(dr).move_to_element(menu).perform()
 time.sleep(2)
