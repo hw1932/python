cd514c2be3f37e3eca96717d224d1182848d6a1f
hw <947412461@qq.com>
2016-08-19 17:08:00 +0800

    1

--- Selenium/007day/mouse_events/drag_drop_0817.py
+++ Selenium/007day/mouse_events/drag_drop_0817.py
@@ -7,13 +7,14 @@ import os
 file_path = 'file:///' + os.path.abspath('drag_drop_0817.html')
 d = webdriver.Firefox()
 d.get(file_path)
+d.implicitly_wait(10)
 
 #定位元素
-source_id = '1'
-target_id = '3'
+source_name = '2'
+target_name = '4'
 try:
-    source = d.find_element_by_id(source_id)
-    target = d.find_element_by_id(target_id)
+    source = d.find_element_by_name(source_name)
+    target = d.find_element_by_name(target_name)
     #source = d.find_element_by_link_text('Python')
     #target = d.find_element_by_link_text('UTF-8')
 except:#定位失败则退出浏览器，并打印失败消息
@@ -23,8 +24,11 @@ else:#定位成功则继续下一步，并打印成功消息
     print('successfully located')
         
 
-
+input('next move')
 #拖动
-ActionChains(d).drag_and_drop(source,target).perform()
+action = ActionChains(d)
+action.drag_and_drop(source,target)
+action.perform()
+#ActionChains(d).drag_and_drop(source,target).perform()
 input('after check out the web page,please press any keys to close the driver')
 d.close()
